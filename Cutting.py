1eb2eb43362b29f062d0d556683766669a3c1ba7
Sovenok-try <kotya.muzykina@mail.ru>
2022-09-08 12:18:40 +0300

    Новый алгоритм нарезки, ускоренный

--- Cutting.py
+++ Cutting.py
@@ -2,9 +2,9 @@ import os
 from libs.Work_With_Images import Processing
 
 
-path_to_image = "D:\\TrainSet\\Sew-Net\\Data-Set Sew\\Cyparis\\m3on2w3_test" #Расположение изображения
-name_of_image = "_sewed_cy2_m3_0088.jpg"
-path_to_dir = "D:\\TrainSet\\Sew-Net\\Data-Set Sew\\Cyparis\\m3on2w3_test\\cy2_2w3_0088" #Папка, куда сохранять нарезанные изображения
+path_to_image = "D:\\TrainSet\\DataSet - Sew [2022]\\Cyparis\\3w4onm3_test" #Расположение изображения
+name_of_image = "cy2_3w4_0316_sewed.jpg"
+path_to_dir = "D:\\Cutting\\Try" #Папка, куда сохранять нарезанные изображения
 image_size = 150 #Размер получившегося изображения
 borderdelta = 0 #Ширина поля перекрытия фрагментов
 
@@ -14,6 +14,7 @@ if not os.path.exists(path_to_image + "\\" + name_of_image):
 else:
     if not os.path.exists(path_to_dir):
         path_to_dir = path_to_image + "\\" + name_of_image[:-4] + "_cut"
-        os.mkdir(path = path_to_image + "\\" + name_of_image[:-4] + "_cut")
+        if not os.path.exists(path_to_dir):
+            os.mkdir(path = path_to_image + "\\" + name_of_image[:-4] + "_cut")
         
     Processing(path_to_image,name_of_image,path_to_dir,image_size,borderdelta)
--- libs/Work_With_Images.py
+++ libs/Work_With_Images.py
@@ -1,25 +1,28 @@
 import numpy as np
 from PIL import Image
+import time
 
 def cut_image(startimg, img_rows, img_cols, chenals, step, wSteps, hSteps):
-    im_width = startimg.size[0]
-    im_height = startimg.size[1]
-    imgPix = startimg.load() #Выгружаются значения пикселей
+    im_width = startimg.shape[0]
+    im_height = startimg.shape[1]
     ni = wSteps*hSteps
     imgs = np.zeros((ni, img_rows, img_cols, chenals))
     print(wSteps, hSteps, ': ',imgs.shape)
-    for w in range(wSteps):
-        for h in range (hSteps):
-            for i in range(img_rows):
-                for j in range (img_cols):
-                    if ((w*step + j) < im_width):
-                        if((h*step + i) < im_height):
-                            if chenals == 1:
-                                imgs[w + h*wSteps,i,j] = imgPix[w*step + j, h*step + i]
-                            else:
-                                for k in range(chenals):
-                                    a = imgPix[w*step + j, h*step + i]
-                                    imgs[w + h*wSteps,i,j,k] = a[k]
+    for col in range(wSteps):
+        for row in range(hSteps):
+            start_wp = col*step
+            end_wp = start_wp + img_cols 
+            start_hp = row*step 
+            end_hp = start_hp + img_rows
+            if end_wp > im_width  and end_hp > im_height:
+                imgs[row + hSteps*col,:(im_width-start_wp),:(im_height-start_hp),:] = startimg[start_wp:end_wp,start_hp:end_hp,:]
+            elif end_wp > im_width:
+                imgs[row + hSteps*col,:(im_width-start_wp),:,:] = startimg[start_wp:end_wp,start_hp:end_hp,:]
+            elif end_hp > im_height:
+                imgs[row + hSteps*col,:,:(im_height-start_hp),:] = startimg[start_wp:end_wp,start_hp:end_hp,:]
+            else:
+                imgs[row + hSteps*col,:,:,:] = startimg[start_wp:end_wp,start_hp:end_hp,:]            
+
     return imgs
 
 def Define_Chenal(startimg):
@@ -40,22 +43,26 @@ def Processing(path_to_image,name_of_image,path_to_dir,image_size,borderdelta):
     img_cols = img_rows #Высота входных изображений НС
 
     step = img_rows -  borderdelta #Шаг смещения при разрезании большой картинки на части img_rows, img_cols - step = области_перекрытия
-
+    print(step)
     startimg = Image.open(path_to_image + "\\" + name_of_image)
     chenals = Define_Chenal(startimg)
+    im_mode = startimg.mode
     print(startimg.size)
-    im_width = startimg.size[0]
-    im_height = startimg.size[1]
+    startimg = np.array(startimg).astype('float32')
+    im_width = startimg.shape[0]
+    im_height = startimg.shape[1]
     wSteps = int(im_width / step ) + 1 #Довольно грубое разрезание, возможно наличие лишней строки/солбца при кратном делении
     hSteps = int(im_height / step) + 1 #но она будет занулённая, так что всё равно..
+    start_time = time.time()
     imgs = cut_image(startimg, img_rows, img_cols, chenals, step, wSteps, hSteps) 
-    print("Изображения нарезаны. Сохраняю в папку: ",path_to_dir)
+    mins,sex = divmod(time.time() - start_time,60)
+    print('Изображения нарезаны - потрачено {M}m : {S:.1f}s. Сохраняю в папку: {P}'.format(M = int(mins), S = sex, P = path_to_dir))
     for i in range(imgs.shape[0]):
         if chenals == 1:
             res = imgs[i].reshape(imgs[i].shape[0], imgs[i].shape[1])
         else:
             res = imgs[i]
-        resimg = Image.fromarray(res.astype('uint8'), mode = startimg.mode)
+        resimg = Image.fromarray(res.astype('uint8'), mode = im_mode)
         resimg = resimg.resize((img_rows,img_cols))
         file_name = path_to_dir + '\\' + str(i+1) + "_" + name_of_image
         resimg.save(file_name)
